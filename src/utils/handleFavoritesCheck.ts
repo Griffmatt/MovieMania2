9ee15e9d4f51823f1cf7496d8aa45ea76950b849
Griffin <griffinmmatthews@gmail.com>
2022-08-31 17:04:07 -0400

    added utils folder and way to handle favorites

---
+++ src/utils/handleFavoritesCheck.ts
@@ -0,0 +1,6 @@
+import {Imovie} from "../typescript/interfaces/movie"
+
+export const handleFavoritesCheck = (movie: Imovie, favorites: Imovie[]) =>{
+    let filtered = favorites.find((favorite: Imovie) => movie.title=== favorite.title)
+    return filtered !== undefined
+  }
