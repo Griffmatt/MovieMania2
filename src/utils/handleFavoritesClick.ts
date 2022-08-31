52d26ba96c1addc87688394c536a6b59d2826141
Griffin <griffinmmatthews@gmail.com>
2022-08-31 17:04:46 -0400

    added check to see if movie is favorited

---
+++ src/utils/handleFavoritesClick.ts
@@ -0,0 +1,14 @@
+import { addFavorite, removeFavorite} from '../redux/favoriteSlice'
+import { Dispatch, AnyAction } from 'redux'
+
+import {Imovie} from "../typescript/interfaces/movie"
+
+export const handleFavoritesClick = (movie: Imovie, favorites: Imovie[], dispatch: Dispatch<AnyAction>) => {
+    let filtered = favorites.find((favorite: Imovie) => movie.title=== favorite.title)
+    console.log(filtered)
+    if(filtered){
+      dispatch(removeFavorite(movie))
+    }else{
+      dispatch(addFavorite(movie))
+    }
+  }
