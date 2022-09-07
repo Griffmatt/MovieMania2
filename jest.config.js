53c1b42830e75fca29e8c5a10abf91f7d755ecc9
Griffin <griffinmmatthews@gmail.com>
2022-09-06 20:51:13 -0400

    added more eslint config jest and cypress

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+
+export const preset = 'ts-jest';
+export const moduleNameMapper = {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+};
+export const testEnvironment = 'jest-environment-jsdom';
