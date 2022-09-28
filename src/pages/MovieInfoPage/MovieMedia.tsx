73571d3224c308e31a8fcbf0ddfb4247167f67fd
Griffin <griffinmmatthews@gmail.com>
2022-09-27 21:26:56 -0400

    updated conditional rendering for images and trailers

--- src/pages/MovieInfoPage/MovieMedia.tsx
+++ src/pages/MovieInfoPage/MovieMedia.tsx
@@ -14,45 +14,52 @@ function MovieMedia({ images, movie, videos }: Props) {
 
   const MovieImages = () => {
     return (
-      <div className="w-full lg:w-[45%] grid gap-2">
-        <h2>Images</h2>
-        <Carousel showThumbs={false} infiniteLoop={true} key={movie.title}>
-          {images?.map((image: Iimage, index: number) => {
-            return (
-              <img
-                key={index}
-                src={`https://image.tmdb.org/t/p/w500${image.file_path}`}
-                alt={movie.title}
-                className="w-full aspect-video"
-              />
-            )
-          })}
-        </Carousel>
-      </div>
+      <>
+        {images.length > 0 && (
+          <div className="w-full lg:w-[45%] grid gap-2">
+            <h2>Images</h2>
+            <Carousel showThumbs={false} infiniteLoop={true} key={movie.title}>
+              {images.map((image: Iimage, index: number) => {
+                return (
+                  <img
+                    key={index}
+                    src={`https://image.tmdb.org/t/p/w500${image.file_path}`}
+                    alt={movie.title}
+                    className="w-full aspect-video"
+                  />
+                )
+              })}
+            </Carousel>
+          </div>
+        )}
+      </>
     )
   }
 
   const MovieTrailer = () => {
     return (
-      <div className=" w-full lg:w-[45%] grid gap-2">
-        <h2>Trailer</h2>
-        <iframe
-          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-          src={`https://www.youtube.com/embed/${trailer?.key}?rel=0`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          className="w-full aspect-video"
-        ></iframe>
-      </div>
+      <>
+        {trailer && (
+          <div className=" w-full lg:w-[45%] grid gap-2">
+            <h2>Trailer</h2>
+            <iframe
+              src={`https://www.youtube.com/embed/${trailer?.key}?rel=0`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className="w-full aspect-video"
+            ></iframe>
+          </div>
+        )}
+      </>
     )
   }
 
   return (
     <div className="flex items-center flex-col lg:flex-row lg:justify-between ">
-      {images.length > 0 && <MovieImages />}
-      {trailer && <MovieTrailer />}
+      <MovieImages />
+      <MovieTrailer />
     </div>
   )
 }
