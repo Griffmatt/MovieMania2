0f730bb1e4d294d9f4563049c596a1c1b9fb0ce3
Griffin <griffinmmatthews@gmail.com>
2022-09-28 22:43:46 -0400

    fixed background holder for images

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -10,22 +10,21 @@ function MovieGrid({ movies }: { movies: Imovie[] }) {
     <div className="grid grid-cols-2 place-items-center gap-5 p-3 md:grid-cols-3 lg:grid-cols-4">
       {movies.map((movie: Imovie, index) => {
         return (
-          <React.Fragment key={movie.id}>
-            <Link
-              to={`/${movie.id}`}
-              className={`bg-bg-secondary dark:bg-bg-secondary-dark ${
-                index === 0 && movie.backdrop_path
-                  ? 'col-span-full aspect-video'
-                  : 'aspect-[2/3]'
-              } w-full`}
-            >
-              {index === 0 && movie.backdrop_path ? (
-                <MovieBanner movie={movie} />
-              ) : (
-                <MoviePoster movie={movie} />
-              )}
-            </Link>
-          </React.Fragment>
+          <Link
+            to={`/${movie.id}`}
+            className={`rounded-xl bg-bg-secondary dark:bg-bg-secondary-dark ${
+              index === 0 && movie.backdrop_path
+                ? 'col-span-full aspect-video'
+                : 'aspect-[2/3]'
+            } w-full`}
+            key={movie.id}
+          >
+            {index === 0 && movie.backdrop_path ? (
+              <MovieBanner movie={movie} />
+            ) : (
+              <MoviePoster movie={movie} />
+            )}
+          </Link>
         )
       })}
     </div>
