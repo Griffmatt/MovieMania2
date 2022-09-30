8c969f643943f2cd14e44b896e0e16e09d48fc68
Griffin <griffinmmatthews@gmail.com>
2022-09-29 22:24:53 -0400

    split search reasults into seperate component

--- src/pages/MovieSearchPage/MovieSearchPage.tsx
+++ src/pages/MovieSearchPage/MovieSearchPage.tsx
@@ -1,5 +1,3 @@
-import MovieRow from '../../components/MovieGrid'
-import { Imovie } from '../../typescript/interfaces/movie'
 import { useNavigate } from 'react-router-dom'
 
 import { useParams } from 'react-router-dom'
@@ -7,27 +5,19 @@ import BackButton from '../../components/ui/BackButton'
 
 import { genres } from '../../shared/genres'
 import Genres from '../../components/Genres'
-import { useQuery } from '@tanstack/react-query'
-import { fetchMovies } from '../../utils/fetchMovies'
+import SearchResults from './SearchResults'
 
 interface Props {
   request: string
 }
 
-interface Data {
-  results: Imovie[]
-}
-
 function MovieSearchPage({ request }: Props) {
   const { value } = useParams()
+  const fetchRequest = value ? `${request}${value}` : `${request}`
+
   const navigate = useNavigate()
 
   let timer: string | number | NodeJS.Timeout | undefined
-  const fetchRequest = value ? `${request}${value}` : `${request}`
-
-  const { data } = useQuery(['movies', fetchRequest], () =>
-    fetchMovies<Data>(fetchRequest)
-  )
 
   const debounce = (value: string) => {
     clearTimeout(timer)
@@ -49,37 +39,25 @@ function MovieSearchPage({ request }: Props) {
   }
   return (
     <>
-      {data && (
-        <>
-          <div className="w-full h-fit sticky top-0  bg-bg-primary dark:bg-bg-primary-dark border-b-2 border-bg-secondary dark:border-bg-secondary-dark">
-            <div className="p-3 md:p-5 flex gap-5 items-center md:w-2/5">
-              <BackButton />
-              <input
-                key={value}
-                type="search"
-                placeholder="Search"
-                defaultValue={findDefaultValue()}
-                className="search-bar"
-                onChange={(event) => debounce(event.target.value)}
-              />
-            </div>
-          </div>
-          <div className="border-bg-secondary dark:border-bg-secondary-dark">
-            <div className="pt-4 pb-2 px-4 w-screen lg:w-full overflow-x-scroll no-scrollbar flex gap-2 lg:flex-wrap lg:mx-auto">
-              <Genres genres={genres} value={value} />
-            </div>
-            {data.results.length === 0 ? (
-              <div className="flex justify-center py-10">
-                <div className="bg-bg-secondary dark:bg-bg-secondary-dark p-3 md:py-10 rounded-2xl text-center grid gap-6 w-fit">
-                  <h3>No movies Were found for {value}</h3>
-                </div>
-              </div>
-            ) : (
-              <MovieRow movies={data.results} />
-            )}
-          </div>
-        </>
-      )}
+      <div className="w-full h-fit sticky top-0  bg-bg-primary dark:bg-bg-primary-dark border-b-2 border-bg-secondary dark:border-bg-secondary-dark">
+        <div className="p-3 md:p-5 flex gap-5 items-center md:w-2/5">
+          <BackButton />
+          <input
+            key={value}
+            type="search"
+            placeholder="Search"
+            defaultValue={findDefaultValue()}
+            className="search-bar"
+            onChange={(event) => debounce(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="border-bg-secondary dark:border-bg-secondary-dark">
+        <div className="pt-4 pb-2 px-4 w-screen lg:w-full overflow-x-scroll no-scrollbar flex gap-2 lg:flex-wrap lg:mx-auto">
+          <Genres genres={genres} value={value} />
+        </div>
+        <SearchResults request={fetchRequest} value={value} />
+      </div>
     </>
   )
 }
---
+++ src/pages/MovieSearchPage/SearchResults.tsx
@@ -0,0 +1,38 @@
+import MovieRow from '../../components/MovieGrid'
+import { useQuery } from '@tanstack/react-query'
+import { fetchMovies } from '../../utils/fetchMovies'
+import { Imovie } from '../../typescript/interfaces/movie'
+
+interface Props {
+  request: string
+  value?: string
+}
+
+interface Data {
+  results: Imovie[]
+}
+
+function SearchResults({ request, value }: Props) {
+  const { data } = useQuery(['movies', request], () =>
+    fetchMovies<Data>(request)
+  )
+  return (
+    <>
+      {data && (
+        <>
+          {data.results.length === 0 ? (
+            <div className="flex justify-center py-10">
+              <div className="bg-bg-secondary dark:bg-bg-secondary-dark p-3 md:py-10 rounded-2xl text-center grid gap-6 w-fit">
+                <h3>No movies Were found for {value}</h3>
+              </div>
+            </div>
+          ) : (
+            <MovieRow movies={data.results} />
+          )}
+        </>
+      )}
+    </>
+  )
+}
+
+export default SearchResults
