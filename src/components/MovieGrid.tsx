6bd929df995c6413e6694b8008abc4f9b1287ede
Griffin <griffinmmatthews@gmail.com>
2022-10-05 17:21:43 -0400

    updated movie grid cols and loading screen height

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -5,7 +5,7 @@ import MoviePoster from './MoviePoster'
 
 function MovieGrid({ movies }: { movies: Imovie[] }) {
   return (
-    <div className="grid grid-cols-3 gap-1 p-2 md:p-4 md:grid-cols-4 lg:grid-cols-5">
+    <div className="grid grid-cols-3 gap-1 p-2 md:p-4 lg:grid-cols-4">
       {movies.map((movie: Imovie) => {
         return (
           <Link
--- src/components/ui/LoadingScreen.tsx
+++ src/components/ui/LoadingScreen.tsx
@@ -1,7 +1,7 @@
 function LoadingScreen() {
   return (
     <div className="bg-bg-primary dark:bg-bg-primary-dark h-screen w-screen fixed top-0 left-0 z-50">
-      <div className="flex justify-center items-center animate-pulse h-screen">
+      <div className="flex justify-center items-center animate-pulse h-[50vh]">
         <h1 className="text-primary font-extrabold text-9xl">M</h1>
       </div>
     </div>
