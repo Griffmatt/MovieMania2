8a17328cb65edac80e38387b1634d32bc7c0eef6
Griffin <griffinmmatthews@gmail.com>
2022-10-05 22:50:35 -0400

    updated type of fetched watch list movies

--- src/fireBaseUtils/fetchWatchList.ts
+++ src/fireBaseUtils/fetchWatchList.ts
@@ -1,12 +1,17 @@
 import { doc, getDoc } from 'firebase/firestore'
 import db from '../firebase'
-import { Imovie } from '../typescript/interfaces/movie'
+
+interface WatchListMovie {
+  id: string
+  title: string
+  poster_path: string
+}
 
 export const fetchWatchList = async (userId: string | null) => {
   if (userId === null) return userId
   const docRef = doc(db, 'watch-list', userId)
   const watchListDoc = await getDoc(docRef)
-  const watchListData = watchListDoc.data() as { watchList: Imovie[] }
+  const watchListData = watchListDoc.data() as { watchList: WatchListMovie[] }
 
   return watchListData.watchList ?? null
 }
